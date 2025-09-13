8c046efa0ae5e419b9bb9f215165a8ae4936e6ca
Aseem Rai <raiaseem25@gmail.com>
2025-09-14 00:04:56 +0545

    featNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { href } from "react-router-dom";
+
+const navItems = [
+  { name: "Home", href: "#herosection" },
+  { name: "About me", href: "#aboutme" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
 
 const Navbar = () => {
-  return <div className="text-xl text-red-500">Navbar</div>;
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.screenY > 10);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  return (
+    <nav
+      className={`fixed w-full z-40 transition-all duration-300 ${
+        isScrolled ? "py-3 bg-background/80" : "py-5"
+      }`}
+    ></nav>
+  );
 };
 
 export default Navbar;
